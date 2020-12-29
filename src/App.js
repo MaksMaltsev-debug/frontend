9a3a19eafdf640aa25c124a8bc2c285cad26d45d
maltsev.m <maltsev.m@dbbest.com>
2020-12-29 20:14:39 +0200

    Front

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css"
+import {BrowserRouter as Router} from "react-router-dom";
+import Layout from "./Components/registration/Layout";
+import RegisterService from "./Components/registration/RegisterService";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function RegistrationForm() {
+    return (
+        <div className={"wrapper"}>
+            <Router>
+                <RegisterService/>
+            </Router>
+        </div>
+    );
+
 }
 
-export default App;
